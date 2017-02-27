ccba9ca1650f2d085405d5eabfc9f1025aa3ba16
Zarovnyy Vladuslav <zarovni03@gmail.com>
2017-02-27 16:44:08 +0200

    Sending messages #2

--- src/main/java/com/survey/softbistro/run/SurveySoftBistroApplication.java
+++ src/main/java/com/survey/softbistro/run/SurveySoftBistroApplication.java
@@ -3,10 +3,16 @@ package com.survey.softbistro.run;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.survey.softbistro.sending.gmail.EmailSender;
+
 @SpringBootApplication
 public class SurveySoftBistroApplication {
 
+	private static EmailSender emailSender;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SurveySoftBistroApplication.class, args);
+		emailSender = new EmailSender("zarovni03@gmail.com", "19991904");
+		emailSender.send("Test", "hello)", "zarovni03@gmail.com", "vitya.semenyuk1995@gmail.com");
 	}
 }
