adac1b6a6bfb338cf67a7c155c09bd5092f9775e
cortes <speedgear250t@gmail.com>
2017-02-27 14:34:24 +0200

    first commit

---
+++ src/main/java/com/survey/softbistro/run/SurveySoftBistroApplication.java
@@ -0,0 +1,12 @@
+package com.survey.softbistro.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveySoftBistroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveySoftBistroApplication.class, args);
+	}
+}
