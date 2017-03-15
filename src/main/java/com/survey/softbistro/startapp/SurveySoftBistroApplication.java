efbfd4665d699c7ce4693e5ca3271a9cd8d1b129
cortes <speedgear250t@gmail.com>
2017-03-15 17:49:48 +0200

    Delete run packages

--- src/main/java/com/survey/softbistro/startapp/SurveySoftBistroApplication.java
+++ src/main/java/com/survey/softbistro/startapp/SurveySoftBistroApplication.java
@@ -6,10 +6,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.survey.softbistro" }, excludeFilters = {
 		@Filter(type = FilterType.ANNOTATION, value = Configuration.class) })
+@PropertySource("classpath:application.properties")
 public class SurveySoftBistroApplication {
 
 	public static void main(String[] args) {
