6c7e70e6dacbeabc1e33647505e104a5a90dfc39
cortes <speedgear250t@gmail.com>
2017-03-21 15:54:20 +0200

    Create token for authorization of client #32

---
+++ src/main/java/com/softbistro/survey/client/auth/service/AuthorizationService.java
@@ -0,0 +1,120 @@
+package com.softbistro.survey.client.auth.service;
+
+import java.util.UUID;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import com.softbistro.survey.client.auth.components.entities.AuthorizedClient;
+import com.softbistro.survey.client.manage.components.entity.Client;
+import com.softbistro.survey.client.manage.service.ClientService;
+import com.softbistro.survey.response.Response;
+
+/**
+ * Service for authorization of client
+ * 
+ * @author cortes
+ *
+ */
+@Service
+public class AuthorizationService {
+
+	private static final String EMPTY_PASSWORD = "";
+	private static final String EXIST_CLIENT = "Wrong password or email";
+	private static Integer timeValidKey = 30;
+
+	@Autowired
+	private AuthorizedClientService authorizedClientService;
+
+	@Autowired
+	private ClientService clientService;
+
+	/**
+	 * Method that do simple authorization of client
+	 * 
+	 * @param client
+	 *            client - information about client
+	 * @return return - status of execution this method
+	 */
+	public Response simpleAthorization(Client client) {
+
+		AuthorizedClient authorizedClient;
+		try {
+
+			Client resultFindClient = (Client) clientService.findClientByEmail(client.getEmail()).getData();
+			String md5HexPassword = DigestUtils.md5Hex(client.getPassword());
+
+			if (resultFindClient.getId() == null && !resultFindClient.getPassword().equals(md5HexPassword)) {
+
+				return new Response(resultFindClient, HttpStatus.OK, EXIST_CLIENT);
+			}
+
+			String uniqueKey = UUID.randomUUID().toString();
+			authorizedClient = new AuthorizedClient(uniqueKey, resultFindClient.getId().toString(), timeValidKey);
+			authorizedClientService.saveClient(authorizedClient);
+
+		} catch (Exception ex) {
+
+			return new Response(null, HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+		}
+		return new Response(authorizedClient.getUniqueKey(), HttpStatus.OK, null);
+	}
+
+	/**
+	 * Method that do authorization with help of information from social
+	 * networks
+	 * 
+	 * @param client
+	 *            client - information about client
+	 * @return return - status of execution this method
+	 */
+	public Response socialAuthorization(Client client) {
+
+		AuthorizedClient authorizedClient;
+		try {
+
+			client.setPassword(EMPTY_PASSWORD);
+			Response saveResponse = clientService.saveSocialClient(client);
+
+			Client resultFindClient = (Client) clientService.findClientByEmail(client.getEmail()).getData();
+
+			if (resultFindClient.getId() == null) {
+
+				return new Response(resultFindClient, HttpStatus.OK, EXIST_CLIENT);
+			}
+
+			String uniqueKey = UUID.randomUUID().toString();
+			authorizedClient = new AuthorizedClient(uniqueKey, resultFindClient.getId().toString(), timeValidKey);
+			authorizedClientService.saveClient(authorizedClient);
+
+		} catch (Exception ex) {
+
+			return new Response(null, HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+		}
+		return new Response(authorizedClient.getUniqueKey(), HttpStatus.OK, null);
+	}
+
+	/**
+	 * Method that check client's token and if it is valid, time of life token
+	 * will continue
+	 * 
+	 * @param token
+	 *            token - token that identify some client
+	 */
+	public boolean checkAccess(String token) {
+
+		AuthorizedClient authorizedClient = authorizedClientService.findClient(token);
+
+		if (authorizedClient == null) {
+
+			return false;
+		}
+
+		authorizedClient.setTimeValidKey(authorizedClient.getTimeValidKey());
+		authorizedClientService.saveClient(authorizedClient);
+		return true;
+	}
+
+}
