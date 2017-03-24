d5a5facd33bdc30cbba46a5a8862ba8dcda25ef2
af150416 <af150416@gmail.com>
2017-03-24 15:15:12 +0200

    docs abilities added

--- src/main/java/com/softbistro/survey/startapp/SwaggerConfig.java
+++ src/main/java/com/softbistro/survey/startapp/SwaggerConfig.java
@@ -2,8 +2,10 @@ package com.softbistro.survey.startapp;
 
 import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,6 +16,11 @@ public class SwaggerConfig {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
+				.paths(PathSelectors.any()).build().apiInfo(metadata());
+	}
+
+	private ApiInfo metadata() {
+		return new ApiInfoBuilder().title("Survey API").description("Documentation for Survey API").version("0.0.4")
+				.build();
 	}
 }
